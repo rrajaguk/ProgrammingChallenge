034ebada4280c4da5e3e45477737f005e6ead1f2
Ronald Rajagukguk <ronald.penalosa@gmail.com>
2013-10-22 21:54:02 +0800

    Finished [1.6.3 The Trip]

---
+++ ProgrammingChallange/Chapter1/TheTrip.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingChallange.Chapter1
+{
+    public class TheTrip
+    {
+        private List<int> toBeOperated;
+        public void setInput(List<int> inputs)
+        {
+            toBeOperated = inputs;
+        }
+
+        public List<int> calculate()
+        {
+            List<int> result = new List<int>();
+
+            // setting up the new trip
+            int pos = 0;
+            int currentval = toBeOperated[pos];
+            
+            while (currentval > 0)
+            {
+                // initialization
+                pos++; 
+                int totalExchange = 0;
+
+                // do a loop to figure out the total cost
+                for (int i = pos; i < pos+currentval; i++)
+                {
+                    totalExchange += toBeOperated[i];
+                }
+                
+                // divide the total cost by number of persons
+                totalExchange = totalExchange / currentval;
+
+                // traverse again to find out how much that person owed
+                int resultVal = 0;
+                for (int i = pos; i < pos + currentval; i++)
+                {
+                    int tempVal =  totalExchange - toBeOperated[i];
+                    // if it's larger than 0 means he's owing the others, add it to money need to be paid
+                    if (tempVal > 0)
+                    {
+                        resultVal += tempVal;
+                    }
+                }
+
+                // add the result
+                result.Add(resultVal);
+
+                // navigate to the next trips
+                pos += (int)(currentval);
+                currentval = toBeOperated[pos];
+
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingChallangeTest/Chapter1/TheTripTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammingChallange.Chapter1;
+
+namespace ProgrammingChallangeTest.Chapter1
+{
+    [TestClass]      
+    public class TheTripTest
+    {
+        [TestMethod]
+        public void TheTrip_Normal_OK()
+        {
+            // arrange
+            TheTrip ts = new TheTrip();
+            List<int> inputs = new List<int>();
+            inputs.Add(3);
+            inputs.Add(1000);
+            inputs.Add(2000);
+            inputs.Add(3000);
+            inputs.Add(4);
+            inputs.Add(1500);
+            inputs.Add(1501);
+            inputs.Add(300);
+            inputs.Add(301);
+            inputs.Add(0);
+            ts.setInput(inputs);
+            
+            // act
+            List<int> results = ts.calculate();
+            List<int> expected = new List<int>() { 1000, 1199 };
+            
+            // assert
+            Assert.AreEqual(expected.Count, results.Count);
+            for (int i = 0; i < expected.Count;  i++)
+            {
+                Assert.AreEqual(expected[i], results[i]);
+                
+            }
+        }
+    }
+}
